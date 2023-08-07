c90759a51c92d4f668935db97f0075e4ba89e0aa
BoboiAzumi <untukherokudeploy@gmail.com>
2023-08-07 20:58:55 +0700

    Gatau Mau Bikin Commit Apa, Yang Penting Ada :D

--- src/Src.go
+++ src/Src.go
@@ -1,16 +1,11 @@
 package src
 
 import (
-	"fmt"
-	httprequest "myanimelisttop/src/HttpRequest"
 	webserver "myanimelisttop/src/WebServer"
 )
 
 
 
 func Run(){
-	var res []byte = httprequest.GetMyAnimeList(1)
 	webserver.WebServerInit()
-
-	fmt.Println(string(res))
 }
